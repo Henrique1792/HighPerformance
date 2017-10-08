e021d804e50b42742f3374dcc9a5840e8402a82f
Henrique Freitas <freitash1792@gmail.com>
2017-10-08 04:09:11 -0300

    slave response added succesfully =D

--- Hello_MPI/hello.c
+++ Hello_MPI/hello.c
@@ -11,7 +11,7 @@
 #define Master(x) x==0 ? 1:0
 
 int main(int argc, char*argv[]){
-    int rank, nprocs, src, dst, tag,pong; 
+    int rank, nprocs, src, dst, tag, ping, pong; 
     char message[]="Hello World";
     char *buff;
     MPI_Status * status;
@@ -22,28 +22,23 @@ int main(int argc, char*argv[]){
     if(Master(rank)){      //rank==0
         MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
         for(dst=1;dst<nprocs;dst++) //Broadcasting
-            MPI_Send(message, sizeof(message)+1, MPI_CHAR,dst, tag, MPI_COMM_WORLD);
+            MPI_Send(message, sizeof(message)+1, MPI_CHAR, dst, tag, MPI_COMM_WORLD);
          
-        tag=2;
-        MPI_Recv(&pong, sizeof(pong), MPI_INT, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD,status);
-        printf("Pong received from: %d\n", src);
-    
+        for(src=1;src<nprocs;src++){
+            MPI_Recv(&pong, 1, MPI_INT, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, status);
+            printf("Pong received from slave!\n");
+        }            
     }else{          //rank!=0
         src=0;
+        ping=1;
         buff=(char *)malloc(sizeof(message)+1); //Receive message
         MPI_Recv(buff,sizeof(message)+1, MPI_CHAR, src, tag, MPI_COMM_WORLD, status);
         printf("%s from master! ID: %d\n", buff, rank);
         free(buff);
         
-        tag=2;
-        pong=1;
         dst=0;
-        MPI_Send(&pong, sizeof(pong), MPI_INT, dst, tag, MPI_COMM_WORLD);
-
+        MPI_Send(message,1 , MPI_CHAR, dst, tag, MPI_COMM_WORLD);
     }
-    
-    
-    
     
     MPI_Finalize();
     return 0;
