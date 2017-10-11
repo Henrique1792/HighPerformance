7d9301b433c70fd78c29146417c7b27818751677
Henrique1792 <freitash1792@gmail.com>
2017-10-11 16:51:03 -0300

    FIXED MY TOKEN RING YAAAAY - WORKING PERFECTLY

--- TokenRing/main.c
+++ TokenRing/main.c
@@ -9,45 +9,69 @@
 
 int main(int argc, char *argv[]){
     //MPI Variables
-        int rank, nprocs, src, dst, flags, message=1, tag;
+        int rank, nprocs, src, dst, flags, message=1, tag,i;
         MPI_Status *status; 
     //MPI Variables
    
     //OMP/MPI required variable
-        int required=MPI_THREAD_FUNNELED, provided;
+        int required=MPI_THREAD_SERIALIZED, provided;
     //OMP/MPI required variables
     
     //OMP Variables
-        
+       int threadID, nthreads; 
     //OMP Variables
     
-    MPI_Init(&argc, &argv);
+    MPI_Init_thread(&argc, &argv,required, &provided);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-   
-    tag=1;
-    if(Master(rank)){
-        MPI_Send(&message, 1, MPI_INT, rank+1, tag, MPI_COMM_WORLD);
-        MPI_Recv(&message, 1, MPI_INT,nprocs-1, tag, MPI_COMM_WORLD, status);
-        printf("Final Message: %d \n",message);
-    }
-    else{
-        MPI_Recv(&message, 1, MPI_INT, rank-1, tag, MPI_COMM_WORLD, status);
-    //OMP code goes here!
-        #pragma omp parallel private (tid) shared(message)\
-        num_threads(nprocs) 
-        {
-            message++;
-        }
-    //OMP code goes here!
-        if((rank+1)==nprocs)
-            MPI_Send(&message, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
-        else
-            MPI_Send(&message, 1, MPI_INT, rank+1, tag, MPI_COMM_WORLD);
-        
-        printf("Message sent from %d \n",rank);
-    } 
+  
     
+    if(provided < required){
+        
+      if (rank == 0) {
+         printf("Warning:  This MPI implementation provides insufficient");
+         printf(" threading support.\n");
+      }
+      nprocs=1;
+        
+    }
+    //creating threads on every proccess
+    {
+        tag=1;
+        if(Master(rank)){
+            MPI_Send(&message, 1, MPI_INT, rank+1, tag, MPI_COMM_WORLD);
+            MPI_Recv(&message, 1, MPI_INT,nprocs-1, tag, MPI_COMM_WORLD, status);
+            printf("Final Message: %d \n",message);
+        }
+        else{
+            #pragma omp parallel num_threads(nprocs) shared(message) private(threadID, i)
+            {
+                //OMP CODE GOES HERE!
+                threadID=omp_get_thread_num();   // Get the thread ID
+                nthreads=omp_get_num_threads();  // Get the total number of threads
+                //OMP code goes here!
+                
+
+                if(Master(threadID)){
+                    MPI_Recv(&message, 1, MPI_INT, rank-1, tag, MPI_COMM_WORLD, status);
+                    #pragma omp critical
+                        for(i=0;i<nprocs;i++)
+                            message++;
+                    #pragma omp barrier
+                    if((rank+1)==nprocs)
+                        MPI_Send(&message, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
+                    else
+                        MPI_Send(&message, 1, MPI_INT, rank+1, tag, MPI_COMM_WORLD);
+                    
+                    printf("Message sent from %d: %d \n",rank, message);
+                }
+                else{
+                    #pragma omp critical
+                        message++;
+                }
+            }
+        } 
+    }    
     
     
     MPI_Finalize();
