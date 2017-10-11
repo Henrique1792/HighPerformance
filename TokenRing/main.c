0fab75a12ba25f4c48a09dc042c8c46950b6f7ef
Henrique Freitas <freitash1792@gmail.com>
2017-10-11 15:24:34 -0300

    Changes on Makefile and main structure for receiving threads

--- TokenRing/main.c
+++ TokenRing/main.c
@@ -1,15 +1,25 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <mpi.h>
 #include <omp.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 
 #define Master(x) x==0 ? 1:0
 
 
 int main(int argc, char *argv[]){
-    int rank, nprocs, src, dst, flags, message=1, tag;
-    MPI_Status *status; 
+    //MPI Variables
+        int rank, nprocs, src, dst, flags, message=1, tag;
+        MPI_Status *status; 
+    //MPI Variables
+   
+    //OMP/MPI required variable
+        int required=MPI_THREAD_FUNNELED, provided;
+    //OMP/MPI required variables
+    
+    //OMP Variables
+        
+    //OMP Variables
     
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -23,7 +33,13 @@ int main(int argc, char *argv[]){
     }
     else{
         MPI_Recv(&message, 1, MPI_INT, rank-1, tag, MPI_COMM_WORLD, status);
-        message++;
+    //OMP code goes here!
+        #pragma omp parallel private (tid) shared(message)\
+        num_threads(nprocs) 
+        {
+            message++;
+        }
+    //OMP code goes here!
         if((rank+1)==nprocs)
             MPI_Send(&message, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
         else
