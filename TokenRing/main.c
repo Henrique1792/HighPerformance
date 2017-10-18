e51596cded93509bd0aa5468a9c76d4d716a5735
Henrique Freitas <freitash1792@gmail.com>
2017-10-18 12:33:54 -0200

    Fixed everything =)

--- TokenRing/main.c
+++ TokenRing/main.c
@@ -13,32 +13,32 @@ int main(int argc, char *argv[]){
     //OMP_VARIABLES
         int tid, nthreads;
     //OMP_VARIABLES
-    int message;
+    int message=0;
     MPI_Init(&argc, &argv);
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
         MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
         omp_set_num_threads(nprocs-1);
         if(Master(rank)){
-            message=0;
-            MPI_Send(&message, 1, MPI_INT, rank+1, tag,MPI_COMM_WORLD);
-            
+            MPI_Send(&message, 1, MPI_INT,rank+1, tag, MPI_COMM_WORLD);
+            MPI_Recv(&message, 1, MPI_INT, nprocs-1, tag,MPI_COMM_WORLD,status);
+            printf("Final message: %d\n", message);
         }else{
             MPI_Recv(&message, 1, MPI_INT, rank-1, tag,MPI_COMM_WORLD,status);
-            #pragma omp parallel default(shared) private(tid,nthreads) 
+            #pragma omp parallel default(shared) private(tid,nthreads) num_threads(nprocs)
             {
                 tid=omp_get_thread_num();
+                nthreads=omp_get_num_threads();
             #pragma omp critical
-                message+=1;
+                message++;
             }
-            message+=1;
             printf("Message from thread %d in %d: %d\n", tid,rank,message);
+            if(!(rank==nprocs-1))
+                MPI_Send(&message, 1, MPI_INT,rank+1, tag, MPI_COMM_WORLD);
+            else
+                MPI_Send(&message, 1, MPI_INT,0, tag, MPI_COMM_WORLD);
         }
             
-        MPI_Send(&message, 1, MPI_INT, (rank+1)%nprocs, tag, MPI_COMM_WORLD);
-        if(Master(rank)){
-            MPI_Recv(&message, 1, MPI_INT, nprocs-1, tag,MPI_COMM_WORLD,status);
-            printf("Final Message: %d\n",message);
-        }
+        
         
     MPI_Finalize();
     
