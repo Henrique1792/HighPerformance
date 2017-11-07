ab36a883d6808a863e45861ac6acc79766424b16
Henrique Freitas <freitash1792@gmail.com>
2017-11-07 01:28:40 -0200

    Makefile Fixed; Commands with "1" runs for 1000 size matrix and so on

--- MPI_OMP_RREF/main.c
+++ MPI_OMP_RREF/main.c
@@ -1,12 +1,22 @@
 #include "utils.h"
-
+#include "matrix.h"
+#define Master(x) x==0 ? 1:0
 
 int main(int argc, char *argv[]){
-  FILE *src;
-  float *teste;
-  int size;
-  
-  MPI_Init(&argc, &argv);
-  MPI_Finalize();
-  return 0;
+    FILE *src;
+    float **matrix;
+    int size;
+    //MPI VARIABLES
+    int rank, nprocs;
+    //MPI VARIABLES
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    //every proccess in MPI will make this allocation
+    matrix=createMatrix(atoi(argv[1]));
+
+
+    freeMatrix(&matrix,atoi(argv[1]));
+    MPI_Finalize();
+    return 0;
 }
--- MPI_OMP_RREF/matrix.c
+++ MPI_OMP_RREF/matrix.c
@@ -4,9 +4,8 @@ float **createMatrix(int size){
     if(size<=0) return NULL;
     float **rt=NULL;
     int i;
-    
+
     rt=(float **)malloc(size*sizeof(float));
-    
     for(i=0;i<size;i++) rt[i]=(float *)malloc(size*sizeof(float));
     return rt;
 }
@@ -14,17 +13,15 @@ float **createMatrix(int size){
 void freeMatrix(float ***tgt,int size){
     if(tgt==NULL || *tgt==NULL || **tgt==NULL || size<=0) return;
     int i;
-    
-    for(i=0;i<size;i++) free(*(tgt[i]));
 
+    for(i=0;i<size;i++) free(*(tgt[i]));
     free(*tgt);
     free(tgt);
-    
+
 }
 void printMatrix(float **tgt,int size){
     int i,j;
     if(tgt==NULL || *tgt==NULL || size<=0) return;
-
     for(i=0;i<size;i++){
         for(j=0;j<size;j++){
             printf("%f ",tgt[i][j]);
@@ -34,14 +31,14 @@ void printMatrix(float **tgt,int size){
 }
 
 void swap(float **A, float **B,int size){
-  if(A==NULL || B==NULL || *A==NULL || *B==NULL || size<=0) return;
-  int i;
-  float tmp;
-  for(i=0;i<size;i++){
-    tmp=*A[i];
-    *A[i]=*B[i];
-    *B[i]=tmp;
-  }
+    if(A==NULL || B==NULL || *A==NULL || *B==NULL || size<=0) return;
+    int i;
+    float tmp;
+    for(i=0;i<size;i++){
+        tmp=*A[i];
+        *A[i]=*B[i];
+        *B[i]=tmp;
+    }
 }
 
 void findSwapLine(float **matrix, int size, int tgt){
